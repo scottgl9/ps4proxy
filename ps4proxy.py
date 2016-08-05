63e8600305de2a2f2ae7f5edee26dea721657c97
¨Scott <scott.glover@r1soft.com>
2016-08-05 16:30:14 -0500

    Updates for testing pkg redirection.

--- ps4proxy.py
+++ ps4proxy.py
@@ -15,26 +15,51 @@ import os.path
 PORT = 8080
 
 class PS4Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    # EPIX app
+    url = 'http://gs2.ww.prod.dl.playstation.net/gs2/appkgo/prod/CUSA00098_00/5/f_192df8f1561054bac199eeb688b2a912df0b3eea7d9bbe40c54008d9a9548b54/f/UT9003-CUSA00098_00-EPIXHD0000001003.json'
+    # grand theft auto III (US)
+    newurl = 'http://gs2.ww.prod.dl.playstation.net/gs2/appkgo/prod/CUSA03508_00/2/f_02db1446c48ce5c6a26c1a9b16c16fcf0f9fd24f2259a9888289ea51dbc33590/f/UP1004-CUSA03508_00-SLUS200620000001.json'
+
     def ishost(self, host):
-	if host in self.path:
-		return self.path
-	return ''
+        if host in self.path:
+                return self.path
+        return ''
+
+    def parse_ps4_url(self):
+        print("handle_ps4_req: " + self.path)
+        base_url = self.path.replace("http://", "")
+        parts = base_url.split('/')
+        filename = parts[8]
+        #pkgfile = filename.replace(".json", ".pkg")
+        print(parts[4])
+        print(parts[6])
+        print(parts[8])
+        #if (os.path.exists(pkgfile)):
+        #    print(pkgfile + " exists on PS4 proxy")
 
     def handle_ps4_req(self):
-	print "handle_ps4_req: " + self.path
-	base_url = self.path.replace("http://", "")
-	parts = base_url.split('/')
-	filename = parts[8]
-	pkgfile = filename.replace(".json", ".pkg")
-	print(parts[4])
-	print(parts[6])
-	print(parts[8])
-	if (os.path.exists(pkgfile)):
-		print(pkgfile + " exists on PS4 proxy")
+        data = ''
+        with urllib.request.urlopen(self.newurl) as response:
+            data = response.read()
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        #print(self.wfile)
+        self.wfile.write("testing")
+        self.wfile.write(data)
+        #self.wfile.write("<html><head><title>Title goes here.</title></head>")
+        #self.wfile.write("<body><p>This is a test.</p>")
+        #self.wfile.write("<p>You accessed path: %s</p>" % self.path)
+        #self.wfile.write("</body></html>")
+        self.wfile.close()
 
     def do_GET(self):
 	if (self.ishost("gs2.ww.prod.dl.playstation.net")):
-		self.handle_ps4_req()
+            print(self.path)
+            self.handle_ps4_req()
+            #self.parse_ps4_url()
+
+        print(self.path)
         self.copyfile(urllib.urlopen(self.path), self.wfile)
 
 httpd = SocketServer.ForkingTCPServer(('', PORT), PS4Proxy)
